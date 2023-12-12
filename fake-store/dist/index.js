4a55a9f25f985190e34e0d693ece679b01bf8edc
Moussa Niang <niangm@ept.sn>
2023-12-12 08:13:44 +0000

    First Commit

---
+++ fake-store/dist/index.js
@@ -0,0 +1,24 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+import { resolvers } from './resolvers.js';
+import { ProductDataSource, CartDataSource, UserDataSource } from './datasources.js';
+// we must convert the file Buffer to a UTF-8 string
+const typeDefs = readFileSync('schema.graphql', 'utf8');
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async ({ req, res }) => {
+        return {
+            dataSources: {
+                cartAPI: new CartDataSource(),
+                productAPI: new ProductDataSource(),
+                userAPI: new UserDataSource(),
+            },
+        };
+    }
+});
+console.log(`🚀  Server ready at: ${url}`);
---
+++ fake-store/src/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+import { resolvers } from './resolvers.js';
+import { ProductDataSource, CartDataSource, UserDataSource } from './datasources.js';
+
+interface ContextValue {
+  dataSources: {
+    cartAPI: CartDataSource,
+    productAPI: ProductDataSource,
+    userAPI: UserDataSource
+  };
+}
+
+// we must convert the file Buffer to a UTF-8 string
+const typeDefs = readFileSync('schema.graphql', 'utf8');
+
+const server = new ApolloServer<ContextValue>({
+    typeDefs,
+    resolvers
+  });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+
+  context: async({req,res}) => {
+    return {
+      dataSources: {
+        cartAPI: new CartDataSource(),
+        productAPI: new ProductDataSource(),
+        userAPI: new UserDataSource(),
+      },
+    };
+
+  }
+});
+  
+console.log(`🚀  Server ready at: ${url}`);
